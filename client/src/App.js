a03791e09f0c3ff98b399b0e50cd6d2fa8288fdb
aishabraud92 <aishabraud@gmail.com>
2018-02-20 22:37:21 -0800

    added nav

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,20 @@ const photos = [
   { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 }
 ];
 
+const trisha = [
+  { src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width: 4, height: 3 },
+  { src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799', width: 1, height: 1 },
+  { src: 'https://source.unsplash.com/qDkso9nvCg0/600x799', width: 3, height: 4 },
+  { src: 'https://source.unsplash.com/iecJiKe_RNg/600x799', width: 3, height: 4 },
+  { src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799', width: 3, height: 4 },
+  { src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599', width: 4, height: 3 },
+  { src: 'https://source.unsplash.com/zh7GEuORbUw/600x799', width: 3, height: 4 },
+  { src: 'https://source.unsplash.com/PpOHJezOalU/800x599', width: 4, height: 3 },
+  { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 }
+];
+
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -36,9 +50,20 @@ class App extends React.Component {
   render() {
     return (
       <div>
+      <div class="container">
+      <h2>My Photo Album</h2>
+        <ul class="breadcrumb">
+          <li><a href="/photos">Home</a></li>
+            <li><a href="/trisha">Trisha</a></li>
+              <li><a href="/test">Pictures</a></li>
+                <li class="/vacation">Vacation</li>
+              </ul>
+            </div>
         <p><button className="toggle-select" onClick={this.toggleSelect}>toggle select all</button></p>
         <Gallery photos={this.state.photos} onClick={this.selectPhoto} ImageComponent={SelectedImage} />
-      </div>
+
+            </div>
+
     )
   }
 }
